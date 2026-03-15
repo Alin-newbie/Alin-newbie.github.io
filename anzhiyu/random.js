3cea6e59ec5303bb28e787dffd0f41811b126507
Alin-newbie <3324779458@qq.com>
2026-03-15 14:03:34 +0800

    Site updated: 2026-03-15 14:03:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/12/Keil5新建工程/","2026/03/02/hello-world/"];function toRandomPost(){
+var posts=["2026/03/02/hello-world/","2026/03/12/Keil5新建工程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
